adc229761605f610c46f920a06972c9a1a71b607
shivammahapatra <31205859+shivammahapatra@users.noreply.github.com>
2017-11-19 15:58:21 +0530

    Update Lab1_question6.cpp

--- Lab1_question6.cpp
+++ Lab1_question6.cpp
@@ -9,7 +9,7 @@ d=r*2;
 p=2*3.14*r;
 a=3.14*r*r;
 cout << "The diameter of the circle=" << d;
-cout << "The perimeter of the circle=" << p;
+cout << "The circumference of the circle=" << p;
 cout << "The area of the circle=" << a;
 }
 
