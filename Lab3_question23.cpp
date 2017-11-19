c5ee42d951b61e9fbd68438554b6c33d5f246f0c
shivammahapatra <31205859+shivammahapatra@users.noreply.github.com>
2017-11-19 18:27:33 +0530

    Update Lab3_question23.cpp

--- Lab3_question23.cpp
+++ Lab3_question23.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 int main()
 {
-int n,x,b;
+int n,b;
  cout<<"Enter Biggest Number of series\n";
  cin>>n;
- x=n;
- while(x>=1)
- { cout<<x<<"\n";
-   x=x-1;
+ while(n>=1)
+ { cout<<n<<"\n";
+   n=n-1;
  }
 }
